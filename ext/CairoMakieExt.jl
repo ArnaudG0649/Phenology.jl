8f1aa60f7576406c6850f550fe671abb5e37d0f2
ArnaudG0649 <arnaudcmc@hotmail.com>
2025-10-31 15:46:07 +0100

    start satisfying CI try 3 (CairoMakieExt fix)

--- ext/CairoMakieExt.jl
+++ ext/CairoMakieExt.jl
@@ -2,6 +2,6 @@ module CairoMakieExt
 
 using Phenology, Dates, DataFrames, Distributions, FileIO, JLD2, CairoMakie
 
-include("phenoplot.jl")
+include(joinpath(@__DIR__,"PhenoPlot.jl"))
 
 end
--- ext/PhenoExt.jl
+++
@@ -1,9 +0,0 @@
-module PhenoExt
-
-using Phenotest
-
-# export Plot_Pheno_Dates_DB_BB, PlotHistogram, Plot_Freeze_Risk, Plot_Freeze_Risk_sample, Plot_Freeze_Risk_Bar
-
-include("PhenoPlot.jl")
-
-end
